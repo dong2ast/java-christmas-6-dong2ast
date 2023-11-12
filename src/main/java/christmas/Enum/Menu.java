0b521112d384258f8d25007a5265f8ffb270d531
dong2ast <kdh01132006@naver.com>
2023-11-12 22:34:41 +0900

    🐛 fix: 메뉴 이름 오타 수정

--- src/main/java/christmas/Enum/Menu.java
+++ src/main/java/christmas/Enum/Menu.java
@@ -2,12 +2,12 @@ package christmas.Enum;
 
 public enum Menu {
     BUTTON_MUSHROOM_SOUP("양송이수프", 6000, 0, MenuType.APPETIZER),
-    TAPAS("바베큐립", 5500, 0, MenuType.APPETIZER),
+    TAPAS("타파스", 5500, 0, MenuType.APPETIZER),
     CAESAR_SALAD("시저샐러드", 8000, 0, MenuType.APPETIZER),
     T_BORN_STEAK("티본스테이크", 55000, 0, MenuType.MAIN),
-    RIB("바베큐립", 54000, 0, MenuType.MAIN),
+    RIB("바비큐립", 54000, 0, MenuType.MAIN),
     SEAFOOD_PASTA("해산물파스타", 35000, 0, MenuType.MAIN),
-    CHRISTMAS_PASTA("해산물파스타", 25000, 0, MenuType.MAIN),
+    CHRISTMAS_PASTA("크리스마스파스타", 25000, 0, MenuType.MAIN),
     CHOCO_CAKE("초코케이크", 15000, 0, MenuType.DESSERT),
     ICE_CREAM("아이스크림", 5000, 0, MenuType.DESSERT),
     ZERO_COKE("제로콜라", 3000, 0, MenuType.DRINK),
