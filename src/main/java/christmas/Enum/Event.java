aa48034ba208b66a35ba4bb6aefbc761bb9bccd8
dong2ast <kdh01132006@naver.com>
2023-11-12 11:52:51 +0900

    ✨ feat: Event Enum 객체 생성

---
+++ src/main/java/christmas/Enum/Event.java
@@ -0,0 +1,26 @@
+package christmas.Enum;
+
+public enum Event {
+    CHRISTMAS("크리스마스 디데이 할인", 100),
+    WEEKDAY("평일 할인", 2023),
+    WEEKEND("주말 할인", 2023),
+    SPECIAL("특별 할인", 1000),
+    FREEBIE("증정 이벤트", 25000),
+    ;
+
+    private final String title;
+    private final Integer rate;
+
+    Event(String title, Integer rate) {
+        this.title = title;
+        this.rate = rate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+}
