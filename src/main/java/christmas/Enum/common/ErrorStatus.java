7da1c7e980e998719afa90f29685a3263157e132
dong2ast <kdh01132006@naver.com>
2023-11-12 15:22:17 +0900

    🐛 fix: Error 메세지 요구사항에 맞게 변경

--- src/main/java/christmas/Enum/common/ErrorStatus.java
+++ src/main/java/christmas/Enum/common/ErrorStatus.java
@@ -1,8 +1,10 @@
 package christmas.Enum.common;
 
 public enum ErrorStatus {
-    PARSE_INT_ERROR("[ERROR] 날짜는 1부터 31 사이의 숫자여야 합니다."),
-    DATE_BOUNDARY_ERROR("[ERROR] 날짜는 1부터 31 사이의 숫자여야 합니다."),
+    PARSE_INT_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요."),
+    DATE_BOUNDARY_ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해주세요."),
+    NO_MENU_ERROR("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    MENU_DUP_ERROR("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ;
 
     private final String message;
