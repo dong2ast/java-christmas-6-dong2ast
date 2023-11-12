2cd20e06f6fc881444ef7ed8e29648f516376faf
dong2ast <kdh01132006@naver.com>
2023-11-12 11:56:20 +0900

    ✨ feat: Badge Enum 객체 생성

---
+++ src/main/java/christmas/Enum/Badge.java
@@ -0,0 +1,24 @@
+package christmas.Enum;
+
+public enum Badge {
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000),
+    ;
+
+    private final String title;
+    private final Integer totalPrice;
+
+    Badge(String title, Integer totalPrice) {
+        this.title = title;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getTotalPrice() {
+        return totalPrice;
+    }
+}
