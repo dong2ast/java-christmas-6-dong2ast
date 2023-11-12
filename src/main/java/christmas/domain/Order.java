768d9b9cb0477f2c1f39ad007ea0a1114b2c6847
dong2ast <kdh01132006@naver.com>
2023-11-12 16:12:55 +0900

    ✨ feat: Order 도메인 구현

---
+++ src/main/java/christmas/domain/Order.java
@@ -0,0 +1,13 @@
+package christmas.domain;
+
+import christmas.Enum.Menu;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private final List<Menu> menu = new ArrayList<>();
+
+    public List<Menu> getMenu() {
+        return menu;
+    }
+}
