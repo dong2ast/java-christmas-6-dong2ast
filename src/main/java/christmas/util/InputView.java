9811e33b43688af51bbf3e054904a1c0e9fa91ed
dong2ast <kdh01132006@naver.com>
2023-11-12 16:11:10 +0900

    ✨ feat: 주문을 받는 로직 구현 + 메뉴와 주문 개수 분리, Order 객체 생성 로직 구현

--- src/main/java/christmas/util/InputView.java
+++ src/main/java/christmas/util/InputView.java
@@ -1,9 +1,21 @@
 package christmas.util;
 
+import static christmas.util.ExceptionModule.*;
+
 import camp.nextstep.edu.missionutils.Console;
-import christmas.Enum.common.ErrorStatus;
+import christmas.Enum.Menu;
+import christmas.domain.Order;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
+
+    private final SplitModule splitModule;
+
+    public InputView() {
+        this.splitModule = new SplitModule();
+    }
+
     public int readDate() throws IllegalArgumentException{
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         Integer date = checkParseIntException(Console.readLine());
@@ -11,17 +23,35 @@ public class InputView {
         return date;
     }
 
-    private Integer checkParseIntException(String consoleRead) throws IllegalArgumentException{
-        try {
-            return Integer.parseInt(consoleRead);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ErrorStatus.PARSE_INT_ERROR.getMessage());
-        }
+    public Order readOrder() throws IllegalArgumentException{
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+
+        List<String> splitOrder = splitModule.splitOrder();
+
+        return splitMenuCount(splitOrder);
     }
 
-    private void checkNumBoundary(Integer num) throws IllegalArgumentException{
-        if (num < 1 || num > 31) {
-            throw new IllegalArgumentException(ErrorStatus.DATE_BOUNDARY_ERROR.getMessage());
+    private Order splitMenuCount(List<String> splitOrder) throws IllegalArgumentException {
+        List<String> menuName = new ArrayList<>();
+        List<Integer> count = new ArrayList<>();
+        int total = 0;
+        for (String s : splitOrder) {
+            String[] split = s.split("-");
+            splitModule.checkMenuName(menuName, split[0]);
+            total += splitModule.checkCount(count, split[1]);
         }
+        ExceptionModule.checkMaxOrderCount(total);
+
+        return makeOrder(menuName, count);
+    }
+
+    private Order makeOrder(List<String> menuName, List<Integer> count) {
+        Order order = new Order();
+        for (int i = 0; i < menuName.size(); i++) {
+            Menu menu = Menu.nameOf(menuName.get(i));
+            menu.order(count.get(i));
+            order.getMenu().add(menu);
+        }
+        return order;
     }
 }
