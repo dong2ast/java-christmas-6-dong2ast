2f08b2d2fbdd742ab1a2bde072f59ba52f27645b
dong2ast <kdh01132006@naver.com>
2023-11-12 17:49:32 +0900

    ✨ feat: 결과 출력 내용에 맞게 Order 객체 필드 추가

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,13 +1,50 @@
 package christmas.domain;
 
+import christmas.Enum.Badge;
+import christmas.Enum.Event;
 import christmas.Enum.Menu;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private final List<Menu> menu = new ArrayList<>();
+    private final List<Menu> orderMenu = new ArrayList<>();
+    private int date;
+    private int discountBeforePrice;
+    private Boolean freebie;
+    private List<Event> event;
+    private int eventPrice;
+    private int discountPrice;
+    private Badge badge;
 
     public List<Menu> getOrderMenu() {
         return orderMenu;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public int getDiscountBeforePrice() {
+        return discountBeforePrice;
+    }
+
+    public Boolean getFreebie() {
+        return freebie;
+    }
+
+    public List<Event> getEvent() {
+        return event;
+    }
+
+    public int getEventPrice() {
+        return eventPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    public Badge getBadge() {
+        return badge;
     }
 }
