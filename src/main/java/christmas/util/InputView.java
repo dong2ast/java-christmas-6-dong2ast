1cc4c3d51dded212a976f4645f10575d24ebcc63
dong2ast <kdh01132006@naver.com>
2023-11-12 12:15:42 +0900

    ✨ feat: 입력 담당 클래스 생성 + 날짜 입력받는 메서드 구현

---
+++ src/main/java/christmas/util/InputView.java
@@ -0,0 +1,27 @@
+package christmas.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.Enum.common.ErrorStatus;
+
+public class InputView {
+    public int readDate() throws IllegalArgumentException{
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        Integer date = checkParseIntException(Console.readLine());
+        checkNumBoundary(date);
+        return date;
+    }
+
+    public Integer checkParseIntException(String consoleRead) throws IllegalArgumentException{
+        try {
+            return Integer.parseInt(consoleRead);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorStatus.PARSE_INT_ERROR.getMessage());
+        }
+    }
+
+    public void checkNumBoundary(Integer num) throws IllegalArgumentException{
+        if (num < 1 || num > 31) {
+            throw new IllegalArgumentException(ErrorStatus.DATE_BOUNDARY_ERROR.getMessage());
+        }
+    }
+}
