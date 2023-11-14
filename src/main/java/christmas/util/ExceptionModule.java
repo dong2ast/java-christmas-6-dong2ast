27b8172e62cbb70bad3fb9288a4328860bc054ec
dong2ast <kdh01132006@naver.com>
2023-11-15 00:22:02 +0900

    🐛 fix: 정규식에 숫자 1자리 이상 들어가게 수정

--- src/main/java/christmas/util/ExceptionModule.java
+++ src/main/java/christmas/util/ExceptionModule.java
@@ -19,7 +19,7 @@ public class ExceptionModule {
             "양송이수프", "타파스", "시저샐러드", "티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타", "초코케이크", "아이스크림", "제로콜라", "레드와인", "샴페인"
     );
 
-    private static final String INPUT_FORMAT = "^([ㄱ-ㅎ|가-힣])+-\\d([, ][ㄱ-ㅎ|가-힣]+-\\d)*";
+    private static final String INPUT_FORMAT = "^([ㄱ-ㅎ|가-힣])+-\\d+([, ][ㄱ-ㅎ|가-힣]+-\\d+)*";
 
     public static Integer checkDayParseIntException(String consoleRead) {
         try {
