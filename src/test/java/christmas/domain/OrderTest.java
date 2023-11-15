85f51cc2469853edbdc27af53821a5a4d83e714f
dong2ast <kdh01132006@naver.com>
2023-11-15 09:42:56 +0900

    ✅ test: 메뉴 개수 계산 테스트 추가

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,6 +1,7 @@
 package christmas.domain;
 
 import christmas.Enum.Menu;
+import christmas.Enum.MenuType;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -31,23 +32,23 @@ public class OrderTest {
     }
 
     @DisplayName("할인 전 총 주문금액 계산 테스트")
-    @Test()
+    @Test
     public void Discount_Before_Price() {
         //given
-        int totalCount = 0;
+        int totalPrice = 0;
         for (int i = 0; i < menu.size(); i++) {
-            totalCount += count.get(i) * menu.get(i).getPrice();
+            totalPrice += count.get(i) * menu.get(i).getPrice();
         }
 
         //when
         int calculateDiscountBeforePrice = order.calculateDiscountBeforePrice();
 
         //then
-        Assertions.assertThat(calculateDiscountBeforePrice).isEqualTo(totalCount);
+        Assertions.assertThat(calculateDiscountBeforePrice).isEqualTo(totalPrice);
     }
 
     @DisplayName("주문 메뉴 출력 테스트")
-    @Test()
+    @Test
     public void Print_Order_Menu() {
         //given
         List<String> target = menu.stream().map(m -> m.getName() + " " + m.getOrderCount() + "개").toList();
@@ -57,5 +58,33 @@ public class OrderTest {
 
         //then
         Assertions.assertThat(orderMenu).isEqualTo(target);
+    }
+
+    @DisplayName("디저트 메뉴 개수 계산 테스트")
+    @Test
+    public void Dessert_Count() {
+        //given
+        int target = menu.stream().filter(m -> m.getMenuType().equals(MenuType.DESSERT))
+                .map(Menu::getOrderCount).mapToInt(Integer::intValue).sum();
+
+        //when
+        int dessertCount = order.getDessertCount();
+
+        //then
+        Assertions.assertThat(dessertCount).isEqualTo(target);
+    }
+
+    @DisplayName("메인 메뉴 개수 계산 테스트")
+    @Test
+    public void Main_Count() {
+        //given
+        int target = menu.stream().filter(m -> m.getMenuType().equals(MenuType.MAIN))
+                .map(Menu::getOrderCount).mapToInt(Integer::intValue).sum();
+
+        //when
+        int mainCount = order.getMainCount();
+
+        //then
+        Assertions.assertThat(mainCount).isEqualTo(target);
     }
 }
