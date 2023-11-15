12a16b10600068cd3c2b1ffda612d5eee3cdf3a4
dong2ast <kdh01132006@naver.com>
2023-11-15 12:01:02 +0900

    ✅ fix: 테스트 실행 전 후로 상태초기화 메서드 추가

--- src/test/java/christmas/domain/EventTargetCheckerTest.java
+++ src/test/java/christmas/domain/EventTargetCheckerTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -21,8 +22,15 @@ import org.junit.jupiter.params.provider.MethodSource;
  */
 public class EventTargetCheckerTest {
 
-    @AfterEach
+    @BeforeEach
     void beforeEach() {
+        for (Event e : Event.values()) {
+            e.check(false);
+        }
+    }
+
+    @AfterEach
+    void afterEach() {
         for (Event e : Event.values()) {
             e.check(false);
         }
